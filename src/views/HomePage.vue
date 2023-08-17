77f2c5c91436fb28eb8dd07887d2b2c14fb5fc4e
powlo <paulcbackhouse@gmail.com>
2023-08-17 12:38:29 +0100

    Migrate to composition api.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,65 +6,57 @@
   </ion-page>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { IonContent, IonPage } from '@ionic/vue';
 import { Geolocation, Position } from '@capacitor/geolocation';
 import { ScreenOrientation, ScreenOrientationResult } from '@capacitor/screen-orientation';
-import { defineComponent } from 'vue';
 import { KeepAwake } from '@capacitor-community/keep-awake';
+import { ref } from 'vue';
 
-export default defineComponent({
-  components: {
-    IonContent,
-    IonPage,
-  },
-  data() {
-    return {
-      mph: NaN as number,
-      dynamicFontSize: this.calculateFontSize() as string
-    }
-  },
-  methods: {
-    calculateFontSize() {
-      // Each char width = .56 x height (NB varies by font)
-      const computedSize = Math.floor((window.innerWidth / 3) * 1.69)
-      return Math.min(computedSize, window.innerHeight) + 'px'
-    },
-    orientationHandler(orientation: ScreenOrientationResult) {
-      // Although orientation has changed, the reported window dimensions will be wrong.
-      // So we add a timeout to ensure a refresh.
-      // NB a vue.nextTick() won't work here, presumably because of
-      // something more low level.
-      setTimeout(() => {
-        this.dynamicFontSize = this.calculateFontSize()
-      }, 1000)
-    },
-    positionHandler(position: Position | null, err: any) {
-      if (err) {
-        console.error(err.message);
-        return;
-      }
-      const speed = position?.coords?.speed
-      const kph = speed ? speed * 60 * 60 / 1000 : NaN;
-      this.mph = Math.round(kph * 0.621371);
-    }
-  },
-  async mounted() {
-    await KeepAwake.keepAwake()
-    const watchOptions = {
-      timeout: 500,
-      enableHighAccuracy: true
-    };
-    Geolocation.watchPosition(watchOptions, this.positionHandler);
-    // Lock orientation because listener doesn't seem to fire
-    // all the time, resulting in display errors. In future add buttons
-    // to explicitly control orientation and mirroring.
-    ScreenOrientation.lock({ orientation: 'landscape-primary' })
-    // Once the lock takes effect the change event will be generated.
-    // Only then can we calculate the font size required.
-    ScreenOrientation.addListener('screenOrientationChange', this.orientationHandler)
+// Do we have to make
+const mph = ref()
+const dynamicFontSize = ref(calculateFontSize())
+
+// await KeepAwake.keepAwake()
+
+function calculateFontSize() {
+  // Each char width = .56 x height (NB varies by font)
+  const computedSize = Math.floor((window.innerWidth / 3) * 1.69)
+  return Math.min(computedSize, window.innerHeight) + 'px'
+}
+
+function orientationHandler(orientation: ScreenOrientationResult) {
+  // Although orientation has changed, the reported window dimensions will be wrong.
+  // So we add a timeout to ensure a refresh.
+  // NB a vue.nextTick() won't work here, presumably because of
+  // something more low level.
+  setTimeout(() => {
+    dynamicFontSize.value = calculateFontSize()
+  }, 1000)
+}
+
+function positionHandler(position: Position | null, err: any) {
+  if (err) {
+    console.error(err.message);
+    return;
   }
-})
+  const speed = position?.coords?.speed
+  const kph = speed ? speed * 60 * 60 / 1000 : NaN;
+  mph.value = Math.round(kph * 0.621371)
+}
+
+const watchOptions = {
+  timeout: 500,
+  enableHighAccuracy: true
+};
+Geolocation.watchPosition(watchOptions, positionHandler);
+// Lock orientation because listener doesn't seem to fire
+// all the time, resulting in display errors. In future add buttons
+// to explicitly control orientation and mirroring.
+ScreenOrientation.lock({ orientation: 'landscape-primary' })
+// Once the lock takes effect the change event will be generated.
+// Only then can we calculate the font size required.
+ScreenOrientation.addListener('screenOrientationChange', orientationHandler)
 </script>
 
 <style scoped>
